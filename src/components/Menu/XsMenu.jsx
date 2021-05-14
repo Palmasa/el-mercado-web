ec17cc8bd52836b0cbc935c90f3c87fbafeee4d0
Ines de Palma <ipalmamasaveu@gmail.com>
2021-05-14 20:27:45 +0200

    Responsive

--- src/components/Menu/XsMenu.jsx
+++ src/components/Menu/XsMenu.jsx
@@ -1,21 +1,22 @@
 import { useRef, useContext } from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { MdClose } from 'react-icons/md'
 import { UserContext } from '../../contexts/UserContext'
 import './XsMenu.scss'
 
 const XsMenu = ({ closeMenu }) => {
+  const modalMenu = useRef();
   const { user } = useContext(UserContext)
-  const modal = useRef(null);
 
-  window.onclick = (event) => {
-    if (event.target === modal.current) {
+  document.onclick = (event) => {
+    console.log(event)
+    if (event.target === modalMenu.current) {
       closeMenu()
     }
   }
 
   return (
-    <div ref={modal} className="overlayMenu">
+    <div ref={modalMenu} className="overlayMenu">
       <div className="popUpMenu">
         <div className="close-menu">
           <button onClick={() => closeMenu()}> <MdClose /></button>
